95760e54793862aefb05c82bd1239309541dcb38
fernandosantoshs <fernandosantoshs@gmail.com>
2022-08-03 10:23:22 -0300

    Delete base64.js

--- base64.js
+++
@@ -1,7 +0,0 @@
-import base64 from 'base-64';
-
-const imgbase64 = 'aHR0cHM6Ly9jb3ZlcnMub3BlbmxpYnJhcnkub3JnL2IvaXNibi85NzgtMDU1MzEwNjYzMy1MLmpwZw==';
-
-let imgDecoded = base64.decode(imgbase64);
-
-console.log(imgDecoded)
